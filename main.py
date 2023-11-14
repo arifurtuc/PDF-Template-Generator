6b4482cde555749b92d10568e867f49162fc1482
arif <arifur.tuc@gmail.com>
2023-11-14 16:03:12 +0100

    Add footer

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 # Create a new PDF instance with A4 size and portrait orientation
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 # Read data from a CSV file
 df = pd.read_csv("files/topics.csv")
@@ -21,9 +22,19 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
 
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
     # Add additional pages to the PDF based on the number of pages required for a topic
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # Set the footer for additional pages
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 # Save the PDF document as output.pdf
 pdf.output("output.pdf")
